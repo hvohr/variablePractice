a59b244a95fc84dfc8b942a18c856894b1706f5e
Hollis Vohr <hollis.vohr@gmail.com>
2023-02-03 14:49:18 -0700

    Practice node with value reassigning

--- variables.js
+++ variables.js
@@ -5,3 +5,14 @@ var age = 23
 var refundAmount = 3500
 var rentPaid = true 
 var online = true 
+
+rentPaid =false
+age = 24
+cityState = "Palm Beach, Florida"
+
+console.log (email)
+console.log (age)
+console.log (rentPaid)
+console.log (middleName)
+console.log (cityState)
+console.log (online)
